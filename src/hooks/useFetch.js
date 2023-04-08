096ef365053c822a6306678c39d8e38e4f3db95e
Rama <ramiroquesadaperez@gmail.com>
2023-04-08 07:22:38 -0300

    api changed, testing for ios

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,6 +3,7 @@ export const getHeroesFetch = async () => {
 	const headersList = {
 		Accept: '*/*',
 		'User-Agent': navigator.userAgent,
+	
 	};
 
 	if (/iPhone|iPad|iPod/.test(navigator.userAgent)) {
@@ -10,7 +11,7 @@ export const getHeroesFetch = async () => {
 	}
 
 	const response = await fetch(
-		'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json',
+		'https://akabab.github.io/superhero-api/api/all.json',
 		{
 			method: 'GET',
 			headers: headersList,
